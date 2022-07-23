5559ee35bb9c8e7c750df16ff225062f0e684699
zanrizkir <sauzan0547@gmail.com>
2022-07-23 17:10:26 +0700

    Menambahkan Register pada Admin LTE

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -9,12 +9,12 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    Selamat, {{Auth::user()->name}} Kamu Berhasil Login
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,7 @@ Route::resource('jurusan', JurusanController::class);
 Route::get('/test-admin', function () {
     return view('layouts.admin');
 });
+
+Route::get('/hello', function () {
+    return view('hello');
+});
